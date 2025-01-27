b77780c62fe31b75f92fd06e793cc292f0f93fa5
박지수 <jisupark2000@gmail.com>
2025-01-27 16:05:04 +0900

    step01 코드 step02로 복사

---
+++ Chapter01/src/step02/TicketSeller.java
@@ -0,0 +1,14 @@
+package step02;
+
+// 티켓 판매원 (매표소 직원)
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
